e5619fc655f3fec1bf0a7909b72f9f7c0b5d5672
Lab User <LabUser@labs.salina.k-state.edu>
2018-09-27 12:12:27 -0500

    9/27/2018

--- models/Author.js
+++ models/Author.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
@@ -22,7 +23,8 @@ AuthorSchema
 AuthorSchema
 .virtual('lifespan')
 .get(function () {
-  return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+  return this.date_of_birth ? moment(this.date_of_birth).format('MMMM Do, YYYY') + " - " + moment(this.date_of_death).format('MMMM Do, YYYY') : '';
+  //return moment(this.date_of_birth).format('MMMM Do, YYYY') + " - " + moment(this.date_of_death).format('MMMM Do, YYYY');  
 });
 
 // Virtual for author's URL
